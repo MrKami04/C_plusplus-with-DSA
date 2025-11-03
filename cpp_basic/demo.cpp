973d7438367cb4febbc6afd5dcd2b76babad6e55
Kamran Manzoor <kamranmanzoor1448@gmail.com>
2025-11-03 12:31:41 +0500

    fisrt commit cpp

---
+++ cpp_basic/demo.cpp
@@ -0,0 +1,8 @@
+// The main difference between C and C++ is that C++ supports classes and objects, while C does not.
+#include <iostream>
+using  namespace std;
+
+int main() {
+    cout<<"Hello World!"<<endl;
+    return 0;
+}
